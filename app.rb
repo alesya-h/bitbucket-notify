91b36fc0b717f8dcedcac917ef1924e7ac65fdbe
Ales Guzik <me@aguzik.net>
2014-06-03 17:51:57 +0300

    show and send get or post params

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
+require "uri"
+require "net/http"
  
-get '/' do
-"Hello world"
+handler = lambda do
+  data = params.inspect
+  form_data = {
+    user:    'ykvA53kfTZmElFLxnfe69HMyGNK61r',
+    message: data,
+    token:   'aFRF8JJgnB5xmdaK37KLh1VkMMFgW6'
+  }
+  Net::HTTP.post_form(URI.parse('https://api.pushover.net/1/messages.json'), form_data)
+  data
 end
+
+get '/', &handler
+post '/', &handler
