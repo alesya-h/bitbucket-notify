df427854d4ffdd93eb803260975080025d4864a0
Ales Guzik <me@aguzik.net>
2014-06-03 18:25:05 +0300

    add bitbucket-specific handler

--- app.rb
+++ app.rb
@@ -1,17 +1,31 @@
 require 'sinatra'
 require "uri"
 require "net/http"
- 
-handler = lambda do
-  data = params.inspect
-  form_data = {
-    user:    'ykvA53kfTZmElFLxnfe69HMyGNK61r',
-    message: data,
-    token:   'aFRF8JJgnB5xmdaK37KLh1VkMMFgW6'
-  }
-  Net::HTTP.post_form(URI.parse('https://api.pushover.net/1/messages.json'), form_data)
-  data
+
+def pushover_settings
+  { user:    'ykvA53kfTZmElFLxnfe69HMyGNK61r',
+    token:   'aFRF8JJgnB5xmdaK37KLh1VkMMFgW6' }
 end
 
-get '/', &handler
-post '/', &handler
+before do
+ request.body.rewind
+ @request_payload = request.body.read
+end
+
+def pushover(message, **args)
+  push_params = pushover_settings.merge(args).merge(message: message)
+  uri = URI.parse('https://api.pushover.net/1/messages.json')
+  Net::HTTP.post_form(uri, push_params)
+end
+
+get '/' do
+  params.inspect.tap {|x| pushover(x) }
+end
+
+post '/' do
+  params.inspect.tap {|x| pushover(x) }
+end
+
+post '/bitbucket' do
+  @request_payload.tap {|x| p pushover(x) }
+end
